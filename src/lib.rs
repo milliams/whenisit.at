a7317ecdcedd1d0ef1b49d37cc1fb4bfe2a8db44
Matt Williams <matt@milliams.com>
2020-09-04 12:29:39 +0100

    Use routing for lookup

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,9 @@ mod datetime;
 
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
+use yew::virtual_dom::VNode;
+use yew_router::{route::Route, service::RouteService, Switch};
+use chrono::TimeZone;
 
 use crate::datetime::DateTime;
 
@@ -17,13 +20,25 @@ use crate::datetime::DateTime;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[derive(Switch)]
+pub enum AppRoute {
+  #[to="/{id}"]
+  GivenTime(chrono::DateTime<chrono::Utc>),
+  #[to="/"]
+  Home,
+}
+
 pub struct Converter {
     link: ComponentLink<Self>,
     datetime: Option<chrono::DateTime<chrono::Utc>>,
+    route_service: RouteService<()>,
+    route: Route<()>,
 }
 
 pub enum Msg {
     DateTimeChanged(chrono::DateTime<chrono::Utc>),
+    RouteChanged(Route<()>),
+    ChangeRoute(AppRoute),
 }
 
 impl Component for Converter {
@@ -31,9 +46,15 @@ impl Component for Converter {
     type Properties = ();
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let mut route_service: RouteService<()> = RouteService::new();
+        let route = route_service.get_route();
+        let callback = link.callback(Msg::RouteChanged);
+        route_service.register_callback(callback);
         Converter {
             link,
             datetime: None,
+            route_service,
+            route,
         }
     }
 
@@ -41,6 +62,12 @@ impl Component for Converter {
         match msg {
             Msg::DateTimeChanged(val) => {
                 self.datetime = Some(val);
+            }
+            Msg::RouteChanged(route) => self.route = route,
+            Msg::ChangeRoute(route) => {
+                // This might be derived in the future
+                self.route = route.into();
+                self.route_service.set_route(&self.route.route, ());
             }
         }
         true
@@ -55,8 +82,20 @@ impl Component for Converter {
         html! {
             <div>
                 <h1>{"When is it at"}</h1>
-                <p>{&if let Some(x) = self.datetime {x.to_string()} else {"".into()} }</p>
-                <DateTime onsignal=onsignal />
+                {
+                    match AppRoute::switch(self.route.clone()) {
+                        Some(AppRoute::Home) => { html! {
+                            <div>
+                                <p>{&if let Some(x) = self.datetime {x.to_string()} else {"".into()} }</p>
+                                <DateTime onsignal=onsignal />
+                            </div>
+                        }},
+                        Some(AppRoute::GivenTime(dt)) => { html! {
+                            <p>{dt.to_string()}</p>
+                        }},
+                        None => VNode::from("404")
+                    }
+                }
             </div>
         }
     }
